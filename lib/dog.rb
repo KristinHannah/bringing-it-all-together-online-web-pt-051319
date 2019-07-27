0a2e3d9d796734d164a316e9f2f71a9c83bf77f0
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-27 16:05:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,8 +79,8 @@ class Dog
   
   def Dog::find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?;"
-    row = DB[:conn].execute(sql, name)
-
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
   end 
   
 end 
