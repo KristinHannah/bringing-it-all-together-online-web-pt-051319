1ed9d1162f8916b6138e806988f2634e85178fee
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-27 15:51:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,10 +53,10 @@ class Dog
   end 
 
   def self.new_from_db(row)
-    dog = self.new 
-    dog.id = row[0]
-    dog.name = row[1]
-    dog.breed = row[2]
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    dog = self.new(name: name, breed: breed, id: id)
     dog
   end 
   
