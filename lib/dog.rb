e6cfe1ffc247cb4e2b3b4a9df96e6d4817d7d4d6
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-27 15:23:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed 
   attr_reader :id
   
-  def initialize(name:, breed:, id: = NIL)
+  def initialize(name, breed, id = NIL)
     @name = name
     @breed = breed 
     @id = id
